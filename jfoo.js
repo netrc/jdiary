11d5a9934e1797e1c2721ff44d5c7322f9874767
netrc <ric@netrc.com>
2017-09-26 19:49:10 -0400

    moved gmStyles to data; lowercase keys; now assign data keys to global

--- jfoo.js
+++ jfoo.js
@@ -89,40 +89,13 @@ function showEntry(jd) {
 	//location.href = "https://codepen.io/netrc/pen/oGgLjN#" + thisDD.date;
 }
 
-var gmStyles = [
-  { "elementType": "geometry", "stylers": [ { "color": "#ebe3cd" } ] },
-  { "elementType": "labels.text.fill", "stylers": [ { "color": "#523735" } ] },
-  { "elementType": "labels.text.stroke", "stylers": [ { "color": "#f5f1e6" } ] },
-  { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [ { "color": "#c9b2a6" } ] },
-  { "featureType": "administrative.land_parcel", "elementType": "geometry.stroke", "stylers": [ { "color": "#dcd2be" } ] },
-  { "featureType": "administrative.land_parcel", "elementType": "labels.text.fill", "stylers": [ { "color": "#ae9e90" } ] },
-  { "featureType": "landscape.natural", "elementType": "geometry", "stylers": [ { "color": "#dfd2ae" } ] },
-  { "featureType": "poi", "elementType": "geometry", "stylers": [ { "color": "#dfd2ae" } ] },
-  { "featureType": "poi", "elementType": "labels.text.fill", "stylers": [ { "color": "#93817c" } ] },
-  { "featureType": "poi.park", "elementType": "geometry.fill", "stylers": [ { "color": "#a5b076" } ] },
-  { "featureType": "poi.park", "elementType": "labels.text.fill", "stylers": [ { "color": "#447530" } ] },
-  { "featureType": "road", "elementType": "geometry", "stylers": [ { "color": "#f5f1e6" } ] },
-  { "featureType": "road.arterial", "elementType": "geometry", "stylers": [ { "color": "#fdfcf8" } ] },
-  { "featureType": "road.highway", "elementType": "geometry", "stylers": [ { "color": "#f8c967" } ] },
-  { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [ { "color": "#e9bc62" } ] },
-  { "featureType": "road.highway.controlled_access", "elementType": "geometry", "stylers": [ { "color": "#e98d58" } ] },
-  { "featureType": "road.highway.controlled_access", "elementType": "geometry.stroke", "stylers": [ { "color": "#db8555" } ] },
-  { "featureType": "road.local", "elementType": "labels.text.fill", "stylers": [ { "color": "#806b63" } ] },
-  { "featureType": "transit.line", "elementType": "geometry", "stylers": [ { "color": "#dfd2ae" } ] },
-  { "featureType": "transit.line", "elementType": "labels.text.fill", "stylers": [ { "color": "#8f7d77" } ] },
-  { "featureType": "transit.line", "elementType": "labels.text.stroke", "stylers": [ { "color": "#ebe3cd" } ] },
-  { "featureType": "transit.station", "elementType": "geometry", "stylers": [ { "color": "#dfd2ae" } ] },
-  { "featureType": "water", "elementType": "geometry.fill", "stylers": [ { "color": "#83B3D4"     //"#99d3c2" } ] },
-  { "featureType": "water", "elementType": "labels.text.fill", "stylers": [ { "color": "#92998d" } ] }
-];
-
 function initMap() {
 	console.log("initMap...");
 	let start = g.views["Start"];
   g.gmap = new google.maps.Map(document.getElementById('mapDiv'), {
       zoom: start.zoom,
 	  mapTypeId: 'terrain',
-      styles: gmStyles,
+      styles: g.gmStyles,
 	  center: start.ll
 		});
 
@@ -153,12 +126,11 @@ function doData(data, status) {
 	if (typeof(data) === "string") {  // local python http doesn't return JSON
 		data = JSON.parse(data);
 	}
-	g.pages = data.PAGES;
-	g.icons = data.ICONS;
-	g.locs = data.LOCS;
-	g.views = data.VIEWS;
+	// put the data into our global 
+	['pages','icons','locs','views','gmStyles'].map( k => g[k] = data[k] );
+
+	// if anchorData is a date, try and set that global currPage in doJSON // TODO
 	anchorData = location.href.replace(/.*#/,"");
-	// if anchorData is a date, try and set that global currPage in doJSON
 	// else start at 0
 	g.jd.forEach(function(jd) {
 		showEntry(jd);
